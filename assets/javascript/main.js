fc0a6e7f8c0537b9f5db7c5e48a51d89a71b4068
Ramon Reyes <reyesjunk@gmail.com>
2018-01-19 08:58:02 -0500

    updated links to open on new tab. Removed carousel code that was causing errors

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,27 +1,3 @@
 $(document).ready(function() { 
-    // Using default configuration 
-
-     // Using custom configuration 
-    $('#carousel').carouFredSel({ 
-        items                : 2, 
-        direction            : "up", 
-        scroll : { 
-            items            : 1, 
-            easing           : "elastic", 
-            duration         : 1000, 
-            pauseOnHover     : true 
-        } 
-    }); 
 }); 
-// $(window).scroll(function(){
-//   var top = $(window).scrollTop();
-//   if(top > 60) {
-//     $("header").addClass('secondry-dark-blue-bg');
-//   }
-
-//   else
-//     if($("header").hasClass('secondary-dark-blue-bg')){
-//       $("header").removeClass('secondary-dark-blue-bg');
-//     }
-// });
 $('#main').stellar();
